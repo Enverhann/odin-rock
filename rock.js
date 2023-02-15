efd8923fbdf499a369895bc219d690b93bfe389d
Enverhann <enverhanoksuz@hotmail.com>
2023-02-15 17:39:03 +0300

    game finished

--- rock.js
+++ rock.js
@@ -1,38 +1,81 @@
-let playerScore = 0;
+        let playerScore = 0;
         let computerScore = 0;
         const choices = ["Rock", "Paper", "Scissor"];
+        const rock = document.querySelector('#rock');
+        const paper = document.querySelector('#paper');
+        const scissor = document.querySelector('#scissor');
+        let playerSelection;
+        let scoreWindow = document.querySelector('#scores');
+        let resultWindow = document.querySelector('#results')
+        let playerWins = document.getElementById('playerwins')
+        let computerWins = document.getElementById('computerwins')
+
+        scoreWindow.innerHTML = "Player" + " : " + playerScore  + " " + "Computer" + " : " +  computerScore;
+        resultWindow.innerHTML = "Result" + " : " ;
 
         function getComputerChoice(){
             const randomChoices = choices[Math.floor(Math.random() * choices.length)];
-            return randomChoices.toLowerCase();
+            console.log(randomChoices);
+            return randomChoices;
         }
-        function getPlayerChoice(){
-            return prompt().toLowerCase();
+        function getPlayerChoice(choose){
+            if(choose == "Rock"){
+                playerSelection = "Rock";
+                console.log(playerSelection);
+                game();
+            }
+            if(choose == "Paper"){
+                playerSelection = "Paper";
+                console.log(playerSelection);
+                game();
+            }
+            if(choose == "Scissor"){
+                playerSelection = "Scissor";
+                console.log(playerSelection);
+                game();
+            }
         }
         function playRound(playerSelection, computerSelection){     
             if(computerSelection == playerSelection){
                 console.log("Tied");
+                resultWindow.innerHTML = "Result" + " : " + "Tied" ;
                 return("You choose:" + playerSelection + " " + "Computer choose:"  + computerSelection);
             }
             else if(
-                (computerSelection=="rock" && playerSelection =="scissor" || computerSelection=="scissor" && playerSelection =="paper" || computerSelection=="paper" && playerSelection =="rock" )){
+                (computerSelection=="Rock" && playerSelection =="Scissor" || computerSelection=="Scissor" && playerSelection =="Paper" || computerSelection=="Paper" && playerSelection =="Rock" )){
                 computerScore ++;
+                scoreWindow.innerHTML = "Player" + " : " + playerScore  + "     " + "Computer" + " : " +  computerScore;
+                resultWindow.innerHTML = "Result" + " : " + "Computer Wins" ;
                 console.log("Computer wins");
+                gameOver();
                 return("You choose:" + playerSelection + " " + "Computer choose:"  + computerSelection);
-            }else if(playerSelection=="rock" && computerSelection =="scissor" || playerSelection=="scissor" && computerSelection =="paper" || playerSelection=="paper" && computerSelection =="rock" ){
+            }else if(playerSelection=="Rock" && computerSelection =="Scissor" || playerSelection=="Scissor" && computerSelection =="Paper" || playerSelection=="Paper" && computerSelection =="Rock" ){
                 playerScore ++;
+                scoreWindow.innerHTML = "Player" + " : " + playerScore  + "     " + "Computer" + " : " +  computerScore;
+                resultWindow.innerHTML = "Result" + " : " + "Player Wins" ;
                 console.log("Player wins");
+                gameOver();
                 return("You choose:" + playerSelection + " " + "Computer choose:"  + computerSelection);
             }
         }
-        game();
-        function game(){
-            for ( let i = 0; i < 5; i ++){
-            const playerSelection=getPlayerChoice();
-            const computerSelection=getComputerChoice(); 
-            const currentRound = playRound(playerSelection, computerSelection);
-            console.log(currentRound);
-            console.log(playerScore, computerScore);
+        function game(){ 
+            if(playerScore < 5 && computerScore < 5){
+                const computerSelection = getComputerChoice();
+                const currentRound = playRound(playerSelection, computerSelection);
+                console.log(currentRound);
+                console.log(playerScore, computerScore);
             }
-        
+            else if(playerScore == 5 || computerScore == 5){
+                return null;
+            } 
         }
+
+        function gameOver(){
+            if(playerScore == 5){
+                playerWins.style.visibility = "visible";
+            }
+            else if(computerScore == 5){
+                computerWins.style.visibility = "visible";
+            }
+        }
+    
